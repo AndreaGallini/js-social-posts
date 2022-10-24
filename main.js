1810d424cfd79b8148d79025c0d9c951c0efc281
AndreaGallini <andrea.gallini01@libero.it>
2022-10-24 14:23:55 +0200

    add main

--- main.js
+++ main.js
@@ -55,3 +55,45 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+const container = document.getElementById('container');
+const arrayNomi = posts.map((val)=> {
+    return val.author
+});
+
+
+posts.forEach(function(value){
+
+    let post = document.createElement('div');
+    post.className = 'post';
+    post.innerHTML = `
+    <div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${value.author.image}" alt="">${value.author.name}">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${value.author.name}</div>
+            <div class="post-meta__time">4 mesi fa</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${value.content}</div>
+<div class="post__image">
+    <img src="${value.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${value.likes}</b> persone
+        </div>
+    </div> 
+</div>`;
+container.append(post)
+})
+
